4ffc10a8f85fce62468f689f0666047556da7bfa
DAQEM <daqemyt@gmail.com>
2024-02-27 21:13:49 +0100

    Initial Commit

---
+++ littlefield.py
@@ -0,0 +1,295 @@
+import requests
+import re
+import os
+
+from collections import namedtuple
+from enum import Enum
+from lxml import etree
+
+session_id_regex = re.compile('JSESSIONID=(\w+);')
+points_regex = re.compile("points: '((?:\d+ [\d.]+ ?)+)'")
+multi_points_regex = re.compile("label: '([\d\w\s]+)', name: '([^']+)', points: '((?:\d+ [\d.]+ ?)+)'")
+orders_info_regex = re.compile(r'''<b>Maximum WIP Limit: </b>(\d+) jobs<BR>
+<B>Number of kits in 1 job: </B>(\d+)<BR>
+<B>Lot size: </B>(\d+) kits, or (\d+) lots? per job<BR>
+<B>Current contract: </B>(\d+)<BR>
+<DD>Quoted lead time: ([\d.]+) day\(s\)<BR>
+<DD>Maximum lead time: ([\d.]+) day\(s\)<BR>
+<DD>Revenue per order: ([\d.]+) dollars<BR><HR>''')
+materials_info_regex = re.compile(r'''<BR><B>Unit Cost: </B> \$ ([\d.]+)
+<BR><B>Order Cost: </B> \$ ([\d.]+)
+<BR><B>Lead Time:</B> (\d+) day\(s\)
+<BR><B>Reorder Point:</B> ([\d,]+) kits
+\( ([\d,]+) batches of (\d+) \)
+<BR><B>Order Quantity:</B>
+([\d,]+) kits
+\( ([\d,]+) batches of (\d+) \)
+(?:<P><B>Material order of ([\d,]+)\s+kits due to arrive in ([\d.]+) simulated days)?''')
+station_info_regex = re.compile(r'''<P><B> Number of Machines: </B>(\d+)<BR>
+<B>Scheduling Policy: </B>(\w+)<BR>
+<B>Purchase Price: </B>\$ ([\d,]+)<BR>
+<B>Retirement Price: </B>\$ ([\d,]+)<BR>''')
+
+
+class Data(Enum):
+    JOBS_IN = 'JOBIN'
+    QUEUED_JOBS = 'JOBQ'
+    INVENTORY = 'INV'
+    STATION1_QUEUE_SIZE = 'S1Q'
+    STATION1_UTILISATION = 'S1UTIL'
+    STATION2_QUEUE_SIZE = 'S3Q'
+    STATION2_UTILISATION = 'S3UTIL'
+    STATION3_QUEUE_SIZE = 'S3Q'
+    STATION3_UTILISATION = 'S3UTIL'
+    COMPLETED_JOB_COUNT = 'JOBOUT'
+    LEAD_TIMES = 'JOBT'
+    REVENUES = 'JOBREV'
+    CASH = 'CASH'
+
+
+OrdersInfo = namedtuple('OrdersInfo',
+                        ['wip_limit', 'kits_per_job', 'lot_size', 'lots_per_job', 'current_contract',
+                         'quoted_lead_time', 'max_lead_time', 'revenue_per_order'])
+
+
+def parse_orders_info(wip_limit, kits_per_job, lot_size, lots_per_job, current_contract, quoted_lead_time,
+                      max_lead_time, revenue_per_order):
+    wip_limit = int(wip_limit)
+    kits_per_job = int(kits_per_job)
+    lot_size = int(lot_size)
+    lots_per_job = int(lots_per_job)
+    current_contract = int(current_contract)
+    quoted_lead_time = float(quoted_lead_time)
+    max_lead_time = float(max_lead_time)
+    revenue_per_order = float(revenue_per_order)
+    return OrdersInfo(wip_limit, kits_per_job, lot_size, lots_per_job, current_contract, quoted_lead_time,
+                      max_lead_time, revenue_per_order)
+
+
+MaterialsInfo = namedtuple('MaterialsInfo',
+                           ['unit_cost', 'order_cost', 'lead_time', 'reorder_point', 'reorder_point_batches',
+                            'reorder_point_batch_size', 'order_quantity', 'order_quantity_batches',
+                            'order_quantity_batch_size',
+                            'next_arrival_quantity', 'next_arrival_eta'])
+
+
+def parse_materials_info(unit_cost, order_cost, lead_time, reorder_point, reorder_point_batches,
+                         reorder_point_batch_size, order_quantity, order_quantity_batches, order_quantity_batch_size,
+                         next_arrival_quantity=None, next_arrival_eta=None):
+    unit_cost = float(unit_cost)
+    order_cost = float(order_cost)
+    lead_time = int(lead_time)
+    reorder_point = int(reorder_point.replace(',', ''))
+    reorder_point_batches = int(reorder_point_batches.replace(',', ''))
+    reorder_point_batch_size = int(reorder_point_batch_size)
+    order_quantity = int(order_quantity.replace(',', ''))
+    order_quantity_batches = int(order_quantity_batches.replace(',', ''))
+    order_quantity_batch_size = int(order_quantity_batch_size)
+    if next_arrival_quantity is not None:
+        next_arrival_quantity = int(next_arrival_quantity.replace(',', ''))
+    if next_arrival_eta is not None:
+        next_arrival_eta = float(next_arrival_eta)
+
+    return MaterialsInfo(unit_cost, order_cost, lead_time, reorder_point, reorder_point_batches,
+                         reorder_point_batch_size, order_quantity, order_quantity_batches, order_quantity_batch_size,
+                         next_arrival_quantity, next_arrival_eta)
+
+
+StationInfo = namedtuple('StationInfo', ['num_machines', 'scheduling_policy', 'purchase_price', 'retirement_price'])
+
+
+def parse_station_info(num_machines, scheduling_policy, purchase_price, retirement_price):
+    num_machines = int(num_machines)
+    purchase_price = float(purchase_price.replace(',', ''))
+    retirement_price = float(retirement_price.replace(',', ''))
+
+    return StationInfo(num_machines, scheduling_policy, purchase_price, retirement_price)
+
+
+class Littlefield:
+    def __init__(self, team_id='', password='', institution=''):
+        if team_id == '' or password == '':
+            self.team_id, self.password = Littlefield._get_credentials_from_environment()
+        else:
+            self.team_id = team_id
+            self.password = password
+            self.institution = institution
+        self.session_id = self._get_session_id()
+
+        self.orders = Orders(self)
+        self.materials = Materials(self)
+        self.station1 = Station(self, 1)
+        self.station2 = Station(self, 2)
+        self.station3 = Station(self, 3)
+        self.completed_jobs = CompletedJobs(self)
+
+    def cash(self, x='all'):
+        return self._get_data(Data.CASH, x)
+    
+    def live_day(self):
+        cash_bytes = self._get_content('LTStatus').splitlines()[-8].split()[-1]
+        cash_str = cash_bytes.decode('utf-8')
+        cash_numeric = ''.join(char for char in cash_str if char.isdigit() or char == ',')
+        return cash_numeric.replace(',', '')
+
+    def live_cash(self):
+        cash_bytes = self._get_content('LTStatus').splitlines()[-6].split()[-1]
+        cash_str = cash_bytes.decode('utf-8')
+        cash_numeric = ''.join(char for char in cash_str if char.isdigit() or char == ',')
+        return cash_numeric.replace(',', '')
+
+
+    @staticmethod
+    def _get_credentials_from_environment():
+        un = os.getenv('LITTLEFIELD_TEAM_ID', '')
+        pw = os.getenv('LITTLEFIELD_PASSWORD', '')
+        return un, pw
+
+    def _get_session_id(self):
+        headers = {'User-Agent': 'PostmanRuntime/7.36.3', 'Content-Type': 'application/x-www-form-urlencoded'}
+        payload = f"institution={self.institution}&ismobile=false&id={self.team_id}&password={self.password}"
+        r = requests.post('https://op.responsive.net/Littlefield/CheckAccess', headers=headers, data=payload)
+        cookie = r.headers.get('Set-Cookie')
+        m = session_id_regex.search(cookie)
+        if m is None:
+            raise RuntimeError(r.text)
+
+        return m.group(1)
+
+    def _get(self, path, params=None):
+        cookie = {'JSESSIONID': self.session_id}
+        r = requests.get('http://op.responsive.net/Littlefield/' + path, cookies=cookie, params=params)
+        return r.text
+    
+    def _get_content(self, path, params=None):
+        cookie = {'JSESSIONID': self.session_id}
+        r = requests.get('http://op.responsive.net/Littlefield/' + path, cookies=cookie, params=params)
+        return r.content
+
+    def _set(self, path, data):
+        cookie = {'JSESSIONID': self.session_id}
+        r = requests.post('http://op.responsive.net/Littlefield/' + path, cookies=cookie, data=data)
+        return r.text
+
+    def _get_data_multi(self, data, x='all'):
+        if isinstance(data, Enum):
+            data = data.value
+
+        raw = self._get('Plot?data={}&x={}'.format(data, x))
+        matches = multi_points_regex.findall(raw)
+        if matches is None:
+            raise RuntimeError('failed to extract data')
+
+        data = [(lbl, name, list(self._to_points(pts))) for lbl, name, pts in matches]
+        return data
+
+    def _get_data(self, data, x='all'):
+        if isinstance(data, Enum):
+            data = data.value
+
+        raw = self._get('Plot?data={}&x={}'.format(data, x))
+        m = points_regex.search(raw)
+        if m is None:
+            raise RuntimeError('failed to extract data')
+
+        data = m.group(1)
+        return list(self._to_points(data))
+
+    @staticmethod
+    def _to_points(data):
+        points = [float(x) for x in data.split(' ')]
+        for i in range(0, len(points), 2):
+            yield points[i], points[i + 1]
+
+
+class Orders:
+    def __init__(self, lf):
+        self.lf = lf
+
+    def job_arrivals(self, x='all'):
+        return self.lf._get_data(Data.JOBS_IN, x)
+
+    def queued_jobs(self, x='all'):
+        return self.lf._get_data(Data.QUEUED_JOBS, x)
+
+    def info(self, update=False) -> OrdersInfo:
+        params = {update: 'update'} if update else None
+        raw = self.lf._get('OrdersMenu', params)
+        m = orders_info_regex.search(raw)
+        if m is None:
+            print(raw)
+            raise RuntimeError('failed to get customer orders')
+
+        return parse_orders_info(*m.groups())
+
+
+class Materials:
+    def __init__(self, lf):
+        self.lf = lf
+
+    def inventory(self, x='all'):
+        return self.lf._get_data(Data.INVENTORY, x)
+
+    def info(self, update=False) -> MaterialsInfo:
+        params = {update: 'update'} if update else None
+        raw = self.lf._get('MaterialMenu', params)
+        m = materials_info_regex.search(raw)
+        if m is None:
+            raise RuntimeError('failed to get material info')
+
+        return parse_materials_info(*m.groups())
+
+    def set_reorder_point(self, r):
+        curr_order_quantity = self.info(True).order_quantity_batches
+        new_val = r * curr_order_quantity
+        data = {
+            'pwd': self.lf.password,
+            'submit': 'confirm',
+            'trans': 'ORDERPOINT,{}'.format(new_val),
+        }
+        return self.lf._set('MaterialForm', data)
+
+    def set_order_quantity(self, q):
+        curr_reorder_point = self.info(True).reorder_point_batches
+        new_val = q * curr_reorder_point
+        data = {
+            'pwd': self.lf.password,
+            'submit': 'confirm',
+            'trans': 'ORDERQUANT,{}'.format(new_val),
+        }
+        return self.lf._set('MaterialForm', data)
+
+
+class Station:
+    def __init__(self, lf, id):
+        self.lf = lf
+        self.id = id
+
+    def queue_size(self, x='all'):
+        return self.lf._get_data('S{}Q'.format(self.id), x)
+
+    def utilization(self, x='all'):
+        return self.lf._get_data('S{}UTIL'.format(self.id), x)
+
+    def info(self, update=False) -> StationInfo:
+        params = {update: 'update'} if update else None
+        raw = self.lf._get('StationMenu?id={}'.format(self.id), params)
+        m = station_info_regex.search(raw)
+        if m is None:
+            raise RuntimeError('failed to get station info')
+
+        return parse_station_info(*m.groups())
+
+
+class CompletedJobs:
+    def __init__(self, lf):
+        self.lf = lf
+
+    def count(self, x='all'):
+        return self.lf._get_data_multi(Data.COMPLETED_JOB_COUNT, x)
+
+    def lead_times(self, x='all'):
+        return self.lf._get_data_multi(Data.LEAD_TIMES, x)
+
+    def revenues(self, x='all'):
+        return self.lf._get_data_multi(Data.REVENUES, x)
---
+++ main.py
@@ -0,0 +1,158 @@
+import nextcord
+import config
+import matplotlib.pyplot as plt
+from littlefield import Littlefield, MaterialsInfo
+from nextcord.ext import commands, tasks
+from nextcord import Intents, Interaction, SlashOption
+
+intents = Intents.all()
+intents.members = True
+
+lf = Littlefield(config.TEAM_NAME, config.TEAM_PASSWORD, config.INSTITUTION)
+lf_day = lf.live_day()
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+    update_rich_presence.start()
+    check_for_new_day.start()
+
+@tasks.loop(seconds=20)
+async def update_rich_presence():
+    await bot.change_presence(activity=nextcord.Game(name="Updating..."))
+    await bot.change_presence(activity=nextcord.Game(name=f"Cash: {lf.live_cash()}"))
+
+
+@tasks.loop(seconds=20)
+async def check_for_new_day():
+    global lf_day
+    if lf.live_day() != lf_day:
+        lf_day = lf.live_day()
+        channel = bot.get_channel(config.CHANNEL_ID)
+        await channel.send(f"Day {lf_day} has started!")
+    
+@bot.command()
+async def ping(ctx):
+    print(ctx)
+
+
+@bot.slash_command(guild_ids=[config.GUILD_ID])
+async def cash(
+    interaction: Interaction
+):
+    try:
+        await interaction.response.send_message("Here is the cash graph:")
+        plot = await plot_data(lf.cash(), 'Cash Flow', 'Day', 'Cash', 'cash_chart.png')
+        await interaction.followup.send(file=plot)
+    except FileNotFoundError:
+        await interaction.response.send_message("The image file 'cash_chart.png' was not found.")
+
+@bot.slash_command(guild_ids=[config.GUILD_ID])
+async def queue_size(
+    interaction: Interaction,
+    station_number: int = SlashOption(name="station", description="The station number", required=True,
+        choices={
+            "Station 1": 1,
+            "Station 2": 2,
+            "Station 3": 3
+        })
+):
+    try:
+        await interaction.response.send_message("Here is the queue size graph for station " + str(station_number) + ":") 
+        station = station_number == 1 and lf.station1 or station_number == 2 and lf.station2 or lf.station3
+        plot = await plot_data(station.queue_size(), f"Station {station_number} Queue Size", 'Day', 'Jobs', 'queue_size_chart.png')
+        await interaction.followup.send(file=plot)
+    except FileNotFoundError:
+        await interaction.response.send_message("The image file 'queue_size_chart.png' was not found.")
+
+@bot.slash_command(guild_ids=[config.GUILD_ID])
+async def utilization(
+    interaction: Interaction,
+    station_number: int = SlashOption(name="station", description="The station number", required=True,
+        choices={
+            "Station 1": 1,
+            "Station 2": 2,
+            "Station 3": 3
+        })
+):
+    try:
+        await interaction.response.send_message("Here is the utilization graph for station " + str(station_number) + ":")
+        station = station_number == 1 and lf.station1 or station_number == 2 and lf.station2 or lf.station3
+        plot = await plot_data(station.utilization(), f"Station {station_number} Utilization", 'Day', 'Utilization', 'utilization_chart.png')
+        await interaction.followup.send(file=plot)
+    except FileNotFoundError:
+        await interaction.response.send_message("The image file 'utilization_chart.png' was not found.")
+
+@bot.slash_command(guild_ids=[config.GUILD_ID])
+async def inventory(
+    interaction: Interaction,
+):
+    await interaction.response.send_message("Here is the inventory info:")
+    info: MaterialsInfo = lf.materials.info()
+
+    embed = nextcord.Embed(title="Materials", description="The current materials in the factory", color=0x00ff00)
+    embed.add_field(name="Next Arrival ETA", value=info.next_arrival_eta or "N/A", inline=False)
+    embed.add_field(name="Next Arrival Quantity", value=info.next_arrival_quantity or "N/A", inline=False)
+    embed.add_field(name="Reorder Point", value=info.reorder_point or "N/A", inline=False)
+    embed.add_field(name="Order Quantity", value=info.order_quantity or "N/A", inline=False)
+    embed.add_field(name="Lead Time", value=info.lead_time or "N/A", inline=False)
+    
+    await interaction.followup.send(embed=embed)
+
+    plot = await plot_data(lf.materials.inventory(), f"Inventory", 'Day', 'Quantity', 'inventory_chart.png')
+    await interaction.followup.send(file=plot)
+
+@bot.slash_command(guild_ids=[config.GUILD_ID])
+async def incoming_jobs(
+    interaction: Interaction,
+):
+    try:
+        await interaction.response.send_message("Here is the incoming jobs graph:")
+        plot = await plot_data(lf.orders.job_arrivals(), 'Incoming Jobs', 'Day', 'Jobs', 'incoming_jobs_chart.png')
+        await interaction.followup.send(file=plot)
+    except FileNotFoundError:
+        await interaction.response.send_message("The image file 'incoming_jobs_chart.png' was not found.")
+
+@bot.slash_command(guild_ids=[config.GUILD_ID])
+async def lead_time(
+    interaction: Interaction,
+):
+    try:
+        await interaction.response.send_message("Here is the lead time graph:")
+        print(lf.completed_jobs.lead_times())
+        plot = await plot_data_multiple(lf.completed_jobs.lead_times(), 'Lead Time', 'Day', 'Time', 'lead_time_chart.png')
+        await interaction.followup.send(file=plot)
+    except FileNotFoundError:
+        await interaction.response.send_message("The image file 'lead_time_chart.png' was not found.")
+
+async def plot_data(data, title, x_label, y_label, file_name):
+    x_values, y_values = zip(*data)
+    plt.plot(x_values, y_values, linestyle='-', color='b')
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.grid(True)
+
+    plt.savefig(file_name)
+
+    with open(file_name, 'rb') as image_file:
+        return nextcord.File(image_file)
+
+async def plot_data_multiple(data, title, x_label, y_label, file_name):
+    for contract, _, points in data:
+        x, y = zip(*points)
+        plt.plot(x, y, label=contract)
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.legend()
+    plt.grid(True)
+
+    plt.savefig(file_name)
+
+    with open(file_name, 'rb') as image_file:
+        return nextcord.File(image_file)
+
+bot.run(config.TOKEN)
